ff9437df7255066c9e2d8b627ce74b4eea966c89
PrinceFroggy <asolesa@yahoo.ca>
2017-04-04 18:58:18 -0400

    GOD
    GOD

--- MazeDrag/GameController.m
+++ MazeDrag/GameController.m
@@ -22,9 +22,9 @@
 
 @property (nonatomic) UIImageView *monster;
 @property int monsterNumber;
+@property int monsterTaps;
 
 @property (nonatomic) UIImageView *draggablePiece;
-@property UIPanGestureRecognizer *pan;
 
 @property NSDictionary *level0;
 @property NSDictionary *level1;
@@ -36,12 +36,12 @@
 @property BOOL timerFlag;
 @property BOOL animationFlag;
 
-// MAP IS RENDERING PIECE AT SECOND TO BE ABOVE YET BEHIND MONSTER
+// MAP IS RENDERING PIECE LAST TO BE ABOVE ASSETS
 
 #define HOME 0
 #define UPGRADE 1
-#define PIECE 2
-#define MONSTER 3
+#define MONSTER 2
+#define PIECE 3
 
 #define EASY_MONSTER 0
 #define MEDIUM_MONSTER 1
@@ -58,7 +58,7 @@
     
     _level0 = @{ @"name" : @"level_0", @"homeX" : @140, @"homeY" : @-10, @"pieceX" : @-140, @"pieceY" : @-10, @"monster": @NO};
     _level1 = @{ @"name" : @"level_1", @"homeX" : @75, @"homeY" : @-195, @"upgrade": @YES, @"upgradeX" : @104, @"upgradeY" : @40, @"pieceX" : @-147, @"pieceY" : @295, @"monster": @NO};
-    _level2 = @{ @"name" : @"level_2", @"homeX" : @75, @"homeY" : @-195, @"upgrade": @YES, @"upgradeX" : @40, @"upgradeY" : @288, @"pieceX" : @-40, @"pieceY" : @290, @"monster": @YES, @"monsters" : @3, @"monster0X": @1, @"monster0Y": @140, @"monster1X": @1, @"monster1Y": @4, @"monster2X": @1, @"monster2Y": @-130 };
+    _level2 = @{ @"name" : @"level_2", @"homeX" : @1, @"homeY" : @-275, @"upgrade": @YES, @"upgradeX" : @40, @"upgradeY" : @288, @"pieceX" : @-40, @"pieceY" : @290, @"monster": @YES, @"monsters" : @3, @"monster0X": @1, @"monster0Y": @140, @"monster1X": @1, @"monster1Y": @4, @"monster2X": @1, @"monster2Y": @-130 };
     _levels = [[NSDictionary alloc] initWithObjectsAndKeys:_level0, @"0", _level1, @"1", _level2, @"2", nil];
     
     _level = 2;
@@ -136,11 +136,6 @@
         [self createUpgradeAtX:newX andY:newY];
     }
     
-    newX = [[self loadPiece:@"X" Piece: PIECE] floatValue];
-    newY = [[self loadPiece:@"Y" Piece: PIECE] floatValue];
-    
-    [self createDraggablePieceAtX:newX andY:newY];
-    
     if ([self.levels valueForKeyPath: [NSString stringWithFormat:@"%d.%@", self.level, @"monster"]])
     {
         for (int monsters = 0; monsters < [[self.levels valueForKeyPath: [NSString stringWithFormat:@"%d.%@", self.level, @"monsters"]] intValue]; monsters++)
@@ -150,13 +145,22 @@
             newX = [[self loadPiece:@"X" Piece: MONSTER] floatValue];
             newY = [[self loadPiece:@"Y" Piece: MONSTER] floatValue];
             
-            //[self createMonsterAtX:newX andY:newY andMonster: (1 + arc4random() % (3 - 1+1))];
+            // RANDOM MONSTERS
+            
+            // [self createMonsterAtX:newX andY:newY andMonster: (1 + arc4random() % (3 - 1+1))];
+            
+            // ORDERED MONSTERS
             
             [self createMonsterAtX:newX andY:newY andMonster: monsters];
             
             [self.monster setTag: monsters + 100];
         }
     }
+    
+    newX = [[self loadPiece:@"X" Piece: PIECE] floatValue];
+    newY = [[self loadPiece:@"Y" Piece: PIECE] floatValue];
+    
+    [self createDraggablePieceAtX:newX andY:newY];
 }
 
 - (void) setDefaultTimer
@@ -253,14 +257,17 @@
     [self.view addConstraint:pieceX];
     [self.view addConstraint:pieceY];
     
-    self.pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panPiece:)];
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panPiece:)];
     
-    [self.draggablePiece addGestureRecognizer:self.pan];
+    [self.draggablePiece addGestureRecognizer:pan];
     
     switch (self.level)
     {
         case 2:
             [self addTapGesture];
+            break;
+            
+        case 3:
             break;
     }
     
@@ -367,8 +374,6 @@
             break;
         }
     }
-    
-    [self.view bringSubviewToFront:self.monster];
 }
 
 - (NSNumber *) loadPiece : (NSString *) XY Piece: (int) piece
@@ -380,20 +385,28 @@
         switch (piece)
         {
             case HOME:
+            {
                 coordinate = [self.levels valueForKeyPath: [NSString stringWithFormat:@"%d.%@", self.level, @"homeX"]];
                 break;
+            }
                 
             case UPGRADE:
+            {
                 coordinate = [self.levels valueForKeyPath: [NSString stringWithFormat:@"%d.%@", self.level, @"upgradeX"]];
                 break;
-                
-            case PIECE:
-                coordinate = [self.levels valueForKeyPath: [NSString stringWithFormat:@"%d.%@", self.level, @"pieceX"]];
-                break;
+            }
                 
             case MONSTER:
+            {
                 coordinate = [self.levels valueForKeyPath: [NSString stringWithFormat:@"%d.%@", self.level, [NSString stringWithFormat:@"monster%dX", self.monsterNumber]]];
                 break;
+            }
+                
+            case PIECE:
+            {
+                coordinate = [self.levels valueForKeyPath: [NSString stringWithFormat:@"%d.%@", self.level, @"pieceX"]];
+                break;
+            }
         }
     }
     else
@@ -401,20 +414,28 @@
         switch (piece)
         {
             case HOME:
+            {
                 coordinate = [self.levels valueForKeyPath: [NSString stringWithFormat:@"%d.%@", self.level, @"homeY"]];
                 break;
+            }
                 
             case UPGRADE:
+            {
                 coordinate = [self.levels valueForKeyPath: [NSString stringWithFormat:@"%d.%@", self.level, @"upgradeY"]];
                 break;
-                
-            case PIECE:
-                coordinate = [self.levels valueForKeyPath: [NSString stringWithFormat:@"%d.%@", self.level, @"pieceY"]];
-                break;
-                
+            }
+             
             case MONSTER:
+            {
                 coordinate = [self.levels valueForKeyPath: [NSString stringWithFormat:@"%d.%@", self.level, [NSString stringWithFormat:@"monster%dY", self.monsterNumber]]];
                 break;
+            }
+                
+            case PIECE:
+            {
+                coordinate = [self.levels valueForKeyPath: [NSString stringWithFormat:@"%d.%@", self.level, @"pieceY"]];
+                break;
+            }
         }
     }
     
@@ -535,7 +556,29 @@
              [self setDefaultTimer];
              self.timerLabel.text = @"";
 
-             [self regenerateUpgrade];
+             if ([self.levels valueForKeyPath: [NSString stringWithFormat:@"%d.%@", self.level, @"upgrade"]])
+             {
+                 [self regenerateUpgrade];
+             }
+             
+             if ([self.levels valueForKeyPath: [NSString stringWithFormat:@"%d.%@", self.level, @"monster"]])
+             {
+                 for (int monsters = 0; monsters < [[self.levels valueForKeyPath: [NSString stringWithFormat:@"%d.%@", self.level, @"monsters"]] intValue]; monsters++)
+                 {
+                     self.monster = [self.view viewWithTag:monsters + 100];
+                     
+                     if (self.level == 2)
+                     {
+                         for (UIGestureRecognizer *gr in self.monster.gestureRecognizers)
+                         {
+                             [self.monster removeGestureRecognizer:gr];
+                         }
+                     }
+                     
+                     [self regenerateMonster:self.monster];
+                 }
+
+             }
              
              CGFloat newX;
              CGFloat newY;
@@ -544,6 +587,8 @@
              newY = [[self loadPiece:@"Y" Piece: PIECE] floatValue];
              
              [self createDraggablePieceAtX:newX andY:newY];
+             
+             [self.view bringSubviewToFront:self.draggablePiece];
              
              self.timerFlag = NO;
              self.animationFlag = NO;
@@ -557,6 +602,14 @@
     if (self.upgrade.alpha == 0.0)
     {
         self.upgrade.alpha = 1.0;
+    }
+}
+
+- (void) regenerateMonster : (UIImageView *) view
+{
+    if (view.alpha == 0.0)
+    {
+        view.alpha = 1.0;
     }
 }
 
@@ -727,7 +780,39 @@
             
         case 2:
         {
-            
+            for (int monsters = 0; monsters < [[self.levels valueForKeyPath: [NSString stringWithFormat:@"%d.%@", self.level, @"monsters"]] intValue]; monsters++)
+            {
+                self.monster = [self.view viewWithTag:monsters + 100];
+                
+                switch (monsters)
+                {
+                    case 0:
+                    {
+                        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapEasyMonster:)];
+                        tap.numberOfTapsRequired = 1;
+                        [self.monster addGestureRecognizer:tap];
+                        break;
+                    }
+                        
+                    case 1:
+                    {
+                        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapMediumMonster:)];
+                        tap.numberOfTapsRequired = 1;
+                        [self.monster addGestureRecognizer:tap];
+                        break;
+                    }
+                        
+                    case 2:
+                    {
+                        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapHardMonster:)];
+                        tap.numberOfTapsRequired = 1;
+                        [self.monster addGestureRecognizer:tap];
+                        break;
+                    }
+                }
+                
+                self.monster.userInteractionEnabled = YES;
+            }
             break;
         }
     }
@@ -739,26 +824,160 @@
     {
         self.monster = [self.view viewWithTag:monsters + 100];
         
-        if (CGRectIntersectsRect(self.draggablePiece.frame, self.monster.frame))
+        if ([self isMonsterAvailable:self.monster])
         {
-            [self pieceDeath];
+            if (CGRectIntersectsRect(self.draggablePiece.frame, self.monster.frame))
+            {
+                [self pieceDeath];
+            }
         }
     }
 }
 
 - (void) tapEasyMonster : (UITapGestureRecognizer *) tap
 {
-    
+    if ([self isMonsterAvailableTapped:tap])
+    {
+        int previousX = self.draggablePiece.frame.origin.x;
+        int previousY = self.draggablePiece.frame.origin.y;
+        
+        if ([self.draggablePiece frame].size.height == 30)
+        {
+            if ([self distanceToView:self.draggablePiece andMonster:tap.view] > 30 && [self distanceToView:self.draggablePiece andMonster:tap.view] < 50)
+            {
+                self.monsterTaps++;
+                
+                [UIImageView animateWithDuration:1.0 animations:^(void)
+                 {
+                     self.draggablePiece.frame = CGRectMake(tap.view.frame.origin.x, tap.view.frame.origin.y, self.draggablePiece.frame.size.width, self.draggablePiece.frame.size.height );
+                 }
+                                      completion:^(BOOL completion)
+                 {
+                     [UIImageView animateWithDuration:1.0 animations:^(void)
+                      {
+                          self.draggablePiece.frame = CGRectMake(previousX, previousY, self.draggablePiece.frame.size.width, self.draggablePiece.frame.size.height );
+                      }
+                                           completion:^(BOOL completion)
+                      {
+                          if (self.monsterTaps == 1)
+                          {
+                              [UIImageView animateWithDuration:2.0 animations:^(void)
+                               {
+                                   tap.view.alpha = 0.0;
+                               }
+                                                    completion:^(BOOL completion)
+                               {
+                                   self.monsterTaps = 0;
+                               }];
+                          }
+                      }];
+                 }];
+            }
+        }
+    }
 }
 
 - (void) tapMediumMonster : (UITapGestureRecognizer *) tap
 {
-    
+    if ([self isMonsterAvailableTapped:tap])
+    {
+        int previousX = self.draggablePiece.frame.origin.x;
+        int previousY = self.draggablePiece.frame.origin.y;
+        
+        if ([self.draggablePiece frame].size.height == 30)
+        {
+            if ([self distanceToView:self.draggablePiece andMonster:tap.view] > 30 && [self distanceToView:self.draggablePiece andMonster:tap.view] < 50)
+            {
+                self.monsterTaps++;
+                
+                [UIImageView animateWithDuration:1.0 animations:^(void)
+                 {
+                     self.draggablePiece.frame = CGRectMake(tap.view.frame.origin.x, tap.view.frame.origin.y, self.draggablePiece.frame.size.width, self.draggablePiece.frame.size.height );
+                 }
+                                      completion:^(BOOL completion)
+                 {
+                     [UIImageView animateWithDuration:1.0 animations:^(void)
+                      {
+                          self.draggablePiece.frame = CGRectMake(previousX, previousY, self.draggablePiece.frame.size.width, self.draggablePiece.frame.size.height );
+                      }
+                                           completion:^(BOOL completion)
+                      {
+                          if (self.monsterTaps == 2)
+                          {
+                              [UIImageView animateWithDuration:2.0 animations:^(void)
+                               {
+                                   tap.view.alpha = 0.0;
+                               }
+                                                    completion:^(BOOL completion)
+                               {
+                                   self.monsterTaps = 0;
+                               }];
+                          }
+                      }];
+                 }];
+            }
+        }
+    }
+
 }
 
 - (void) tapHardMonster : (UITapGestureRecognizer *) tap
 {
-    
+    if ([self isMonsterAvailableTapped:tap])
+    {
+        int previousX = self.draggablePiece.frame.origin.x;
+        int previousY = self.draggablePiece.frame.origin.y;
+        
+        if ([self.draggablePiece frame].size.height == 30)
+        {
+            if ([self distanceToView:self.draggablePiece andMonster:tap.view] > 30 && [self distanceToView:self.draggablePiece andMonster:tap.view] < 50)
+            {
+                self.monsterTaps++;
+                
+                [UIImageView animateWithDuration:1.0 animations:^(void)
+                 {
+                     self.draggablePiece.frame = CGRectMake(tap.view.frame.origin.x, tap.view.frame.origin.y, self.draggablePiece.frame.size.width, self.draggablePiece.frame.size.height );
+                 }
+                                      completion:^(BOOL completion)
+                 {
+                     [UIImageView animateWithDuration:1.0 animations:^(void)
+                      {
+                          self.draggablePiece.frame = CGRectMake(previousX, previousY, self.draggablePiece.frame.size.width, self.draggablePiece.frame.size.height );
+                      }
+                                           completion:^(BOOL completion)
+                      {
+                          if (self.monsterTaps == 3)
+                          {
+                              [UIImageView animateWithDuration:2.0 animations:^(void)
+                               {
+                                   tap.view.alpha = 0.0;
+                               }
+                                                    completion:^(BOOL completion)
+                               {
+                                   self.monsterTaps = 0;
+                               }];
+                          }
+                      }];
+                 }];
+            }
+        }
+    }
+
+}
+
+- (BOOL) isMonsterAvailable: (UIImageView *) view
+{
+    return view.alpha == 1.0 ? YES : NO;
+}
+
+- (BOOL) isMonsterAvailableTapped: (UITapGestureRecognizer *) tap
+{
+    return tap.view.alpha == 1.0 ? YES : NO;
+}
+
+-(double)distanceToView:(UIImageView *)piece andMonster: (UIView *) monster
+{
+    return sqrt(pow(piece.frame.origin.x - monster.frame.origin.x, 2) + pow(piece.frame.origin.y - monster.frame.origin.y, 2));
 }
 
 - (IBAction)resumeGame:(UIButton *)sender
